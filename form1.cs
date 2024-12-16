60358c4086d069e8e2891965ffa1f604e4ab3034
pbuii <phatbui.31231023065@st.ueh.edu.vn>
2024-12-16 20:40:37 +0700

    form1.cs bản cuối

--- form1.cs
+++ form1.cs
@@ -4,6 +4,8 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Media;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,166 +14,313 @@ namespace CSLT
 {
     public partial class Form1 : Form
     {
-        PictureBox organicCan, plasticCan, otherCan;
-        int currentCanIndex = 0;
-        int[] canIndices = { 0, 1, 2 };
-        bool goLeft, goRight;
-        int playerSpeed = 14;
-        int rubbishSpeed = 8;
-        int score = 0;
-        int missed = 0
+        // Khai báo các biến trạng thái trò chơi và điều khiển người chơi
+        bool isGameStarted = false; 
+        bool goLeft, goRight; 
+        int playerSpeed = 14; 
+        int rubbishSpeed = 3; 
+        int score; 
+        int missed; 
 
-        Random random = new Random();
+        // Khai báo các biến cho các loại thùng rác
+        int canType; 
+        private Image can1; 
+        private Image can2; 
+        private Image can3;
 
+        // Khai báo các biến cho các loại rác khác nhau
+        private Image r1; 
+        private Image r2; 
+        private Image r3; 
+        private Image r4; 
+
+        private Image nr1; 
+        private Image nr2; 
+        private Image nr3; 
+        private Image nr4; 
+
+        private Image o1; 
+        private Image o2; 
+        private Image o3; 
+        private Image o4; 
+
+        // Khai báo mảng 2 chiều để lưu trữ các kết hợp hình ảnh rác khác nhau
+        private Image[,] rubbishes; 
+
+        // Khai báo đối tượng phát âm thanh hiệu ứng và nhạc nền
+        private SoundPlayer catchSound; 
+        private SoundPlayer missSound; 
+        private SoundPlayer backgroundMusic; 
+
+        // Tạo một đối tượng số ngẫu nhiên
+        Random random = new Random(); 
         public Form1()
         {
             InitializeComponent();
             restartGame();
         }
 
-        private voide Form1_Load(object sender, EventArgs e) 
+        // Xử lý sự kiện click nút "Bắt đầu"
+        private void Start_Click(object sender, EventArgs e)
         {
-            // Load image
-            Image plastic1 = Image.FromFile(@"CSLT\\Images\\plastic1");
-            Image plastic2 = Image.FromFile(@"CSLT\\Images\\plastic2");
-            Image plastic3 = Image.FromFile(@"CSLT\\Images\\plastic3");
-            Image organic1 = Image.FromFile(@"CSLT\\Images\\organic1");
-            Image organic2 = Image.FromFile(@"CSLT\\Images\\organic2");
-            Image organic3 = Image.FromFile(@"CSLT\\Images\\organic3");
-            Image other1 = Image.FromFile(@"CSLT\\Images\\other1");
-            Image other2 = Image.FromFile(@"CSLT\\Images\\other2");
-            Image other3 = Image.FromFile(@"CSLT\\Images\\other3");
-
-            // Load sound
-            SoundPlayer backgroundMusic = new SoundPlayer("background.mp3");
-            SoundPlayer catchSound = new SoundPlayer("catch.wav");
-            SoundPlayer missSound = new SoundPlayer("miss2.wav");
-
-            // Image 2d array
-            Image[,] ImageArray = {{plastic1, plastic2, plastic3}, {organic1, organic2, organic3}, {other1, other2, other3}};
-        }
-        
-        private int GetRubbishType(PictureBox rubbish)
-        {
-            string[] tagParts = ((string)rubbish.Tag).Split(':');
-            return int.Parse(tagParts[1]);
-        }   
-        private void MainGameTimerEvent(object sender, EventArgs e) 
-        {
-            if (goLeft && UEher.Left >= 5)
+            // Kiểm tra nếu trò chơi chưa bắt đầu
+            if (!isGameStarted)
             {
-                UEHer.Left -= playerSpeed;
-            }
-            if (goRight && UEHer.Left <= this.ClientSize.Width - UEHer.Width - 5)
-            {
-                UEHer.Left += playerSpeed;
-            }
+                // Phát nhạc nền
+                backgroundMusic.Play();
 
-            foreach (Control x in this.Controls)
-            {
-                if (x is PictureBox && (string).x.Tag == "rubbish")
-                {
-                    x.Top += rubbishSpeed;
+                // Giả sử pictureBox2 là ảnh nút "Bắt đầu", ẩn nó đi
+                Sorting.Visible = false;
 
-                    if (x.Top + x.Height >= this.ClientSize.Height)
-                    {
-                        X.Top = random.Next(80,300) * -1;
-                        X.Left = random.Next(5, this.ClientSize.Width - 5 - x.Width)
-                    }
-                    if (x.Bounds.IntersectsWith(Controls[canIndices[currentCanIndex]].Bounds) && GetRubbishType(x) == currentCanIndex)
-                    {
-                        // Correct trash can, increase score
-                        score++;
-                        x.Top = random.Next(80, 300) * -1;
-                        x.Left = random.Next(5, this.ClientSize.Width - 5 - x.Width);
-                        catchSound.Play();
-                    }
-                    else if (x.Top + x.Height >= this.ClientSize.Height || GetRubbishType(x) != currentCanIndex)
-                    {
-                        // Missed the trash, decrease score or increase missed count
-                        missed++;
-                        // reset rubbish position
-                        x.Top = random.Next(80, 300) * -1;
-                        x.Left = random.Next(5, this.ClientSize.Width - 5 - x.Width);
-                        missSound.Play();
-                    }
-                }
-                scoreLabel.Text = "Score: " + score;
+                // Ẩn nút "Bắt đầu"
+                Start.Visible = false;
+
+                // Thiết lập trạng thái trò chơi thành đã bắt đầu
+                isGameStarted = true;
+
+                // Gọi hàm restartGame() để khởi tạo lại trò chơi
+                restartGame();
+
+                // Bắt đầu bộ hẹn giờ trò chơi
+                MainTimer.Start();
             }
         }
-
-        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        // Xử lý sự kiện hẹn giờ chính của trò chơi (được gọi mỗi khi hẹn giờ chạy)
+        private void MainGameTimerEvent(object sender, EventArgs e)
         {
-            if (e.KeyCode == Keys.Left)
+            // Kiểm tra nếu trò chơi đã bắt đầu
+            if (isGameStarted)
             {
-                if (UEHer.Left >= 5)
+                // Xử lý di chuyển của người chơi dựa trên các phím được nhấn
+                if (goLeft && UEHer.Left >= 5) // Kiểm tra nếu phím trái được nhấn và còn đủ chỗ để di chuyển sang trái
                 {
-                    goLeft = true;
+                    UEHer.Left -= playerSpeed; // Di chuyển hình ảnh người chơi sang trái
+                }
+                if (goRight && UEHer.Left <= this.ClientSize.Width - UEHer.Width - 5) // Kiểm tra nếu phím phải được nhấn và còn đủ chỗ để di chuyển sang phải
+                {
+                    UEHer.Left += playerSpeed; // Di chuyển hình ảnh người chơi sang phải
+                }
+
+                // Lặp qua tất cả các PictureBox có tag "rubbish"
+                foreach (Control x in this.Controls)
+                {
+                    if (x is PictureBox && ((string)x.Tag == "rubbish:0") || ((string)x.Tag == "rubbish:1") || ((string)x.Tag == "rubbish:2")) // Kiểm tra xem control x có phải PictureBox với tag "rubbish" và loại rác cụ thể
+                    {
+                        ((PictureBox)x).Top += rubbishSpeed; // Di chuyển hình ảnh rác xuống theo tốc độ rác
+
+                        // Lấy loại rác từ tag
+                        int rubbishtype;
+                        bool boo = int.TryParse(((string)x.Tag).Split(':')[1], out rubbishtype);
+
+                        // Kiểm tra xem rác đã ra khỏi màn hình (phía dưới của form) chưa
+                        if (((PictureBox)x).Top > this.ClientSize.Height)
+                        {
+                            ((PictureBox)x).Top = random.Next(80, 300) * -1; // Vị trí lại hình ảnh rác ngoài màn hình ở phía trên
+                            ((PictureBox)x).Left = random.Next(5, this.ClientSize.Width - 5 - ((PictureBox)x).Width); // Đặt một vị trí ngang mới ngẫu nhiên cho rác
+                            ((PictureBox)x).BackColor = Color.Transparent; // Làm cho hình ảnh rác trong suốt trở lại
+                            missed++; // Tăng số lần bỏ lỡ
+                        }
+
+                        // Kiểm tra va chạm giữa người chơi và rác
+                        if (UEHer.Bounds.IntersectsWith(x.Bounds))
+                        {
+                            if (rubbishtype == canType) // Kiểm tra xem loại rác có khớp với loại lon của người chơi không
+                            {
+                                score++; // Tăng điểm nếu bắt đúng
+                                catchSound.Play(); // Phát hiệu ứng âm thanh bắt được
+                            }
+                            else
+                            {
+                                missed++; // Tăng số lần bỏ lỡ nếu bắt sai
+                                missSound.Play(); // Phát hiệu ứng âm thanh bỏ lỡ
+                            }
+
+                            // Cập nhật hình ảnh rác với loại và hình ảnh mới ngẫu nhiên
+                            int type = random.Next(0, 3); // Lấy một loại rác ngẫu nhiên (0, 1 hoặc 2)
+                            int item = random.Next(0, 4); // Lấy một biến thể vật phẩm rác ngẫu nhiên (0, 1, 2 hoặc 3)
+                            ((PictureBox)x).Tag = $"rubbish:{type}"; // Cập nhật tag với loại rác mới
+                            ((PictureBox)x).Image = rubbishes[type, item]; // Đặt hình ảnh PictureBox thành loại rác và biến thể tương ứng
+
+                            ((PictureBox)x).Top = random.Next(80, 300) * -1; // Vị trí lại hình ảnh rác ngoài màn hình ở phía trên
+                            ((PictureBox)x).Left = random.Next(5, this.ClientSize.Width - 5 - x.Width); // Đặt một vị trí ngang mới ngẫu nhiên cho rác
+                            x.BackColor = Color.Transparent; // Làm cho hình ảnh rác trong suốt trở lại
+                        }
+                    }
+                }
+                // Cập nhật nhãn điểm và số lần bỏ lỡ
+                txtScore.Text = "Score: " + score;
+                txtMissed.Text = "Missed: " + missed;
+
+                // Kiểm tra điều kiện kết thúc trò chơi (bỏ lỡ 10 lần)
+                if (missed == 10)
+                {
+                    MainTimer.Enabled = false; // Dừng bộ hẹn giờ trò chơi
+                    GameOver.Visible = true; // Hiển thị nhãn "Game Over"
+                    FinalScore.Text = "Your score is: " + score; // Cập nhật văn bản điểm cuối cùng
+                    FinalScore.Visible = true; // Hiển thị văn bản điểm cuối cùng
+                    Exit.Visible = true; // Hiển thị nút "Thoát"
                 }
             }
-            if (e.KeyCode == Keys.Right)
-            {
-                if (UEHer.Left <= this.ClientSize.Width - 5)
-                {
-                    goRight = true;
-                }
-            }
-                if (e.KeyCode == Keys.D1)
-                    {
-                        currentCanIndex = 0;
-                    }
-                else if (e.KeyCode == Keys.D2)
-                    {
-                        currentCanIndex = 1;
-                    }
-                else if (e.KeyCode == Keys == Keys.D3)
-                    {
-                        currentCanIndex = 2;
-                    }
         }
 
-
+        // Xử lý sự kiện khi người dùng nhả phím
         private void Form1_KeyUp(object sender, KeyEventArgs e)
         {
+            // Kiểm tra nếu phím bấm là phím mũi tên phải
             if (e.KeyCode == Keys.Right)
             {
+                // Đặt cờ di chuyển sang phải về false
                 goRight = false;
             }
+
+            // Kiểm tra nếu phím bấm là phím mũi tên trái
             if (e.KeyCode == Keys.Left)
             {
+                // Đặt cờ di chuyển sang trái về false
                 goLeft = false;
             }
         }
 
-        private void restartGame()
+        // Xử lý sự kiện khi người dùng nhấn phím
+        private void Form1_KeyDown_1(object sender, KeyEventArgs e)
         {
-            // Play background music continuously
-            backgroundMusic.PlayLooping();
-            
-            foreach (Control x in this.Controls) 
+            // Kiểm tra nếu phím trái được nhấn
+            if (e.KeyCode == Keys.Left)
             {
-                if(x is PictureBox && (string)x.Tag == "rubbish") 
+                // Kiểm tra xem còn đủ khoảng trống để di chuyển sang trái
+                if (UEHer.Left >= 5)
                 {
-                    // Access a random rubbish item
-                    int type = random.Next(0, 3);
-                    int item = random.Next(0, 3);
-                    x.Tag = $"rubbish:{type}"                    
-                    x.Image = ImageArray[type, item]
-                    
-                    // Random location
-                    x.Top = random.Next(80,300) * -1;
-                    x.Left = random.Next(5, this.ClientSize.Width - 5 - x.Width);
+                    // Đặt cờ hiệu di chuyển sang trái
+                    goLeft = true;
                 }
             }
 
-            UEHer.Left = this.ClientSize.Width / 2;
-            playerSpeed = 8;
-            currentCanIndex = 0;
-            
+            // Kiểm tra nếu phím phải được nhấn
+            if (e.KeyCode == Keys.Right)
+            {
+                // Kiểm tra xem còn đủ khoảng trống để di chuyển sang phải
+                if (UEHer.Left <= this.ClientSize.Width - UEHer.Width - 5)
+                {
+                    // Đặt cờ hiệu di chuyển sang phải
+                    goRight = true;
+                }
+            }
+
+            // Xử lý khi người dùng chọn loại thùng rác 1 (D1)
+            if (e.KeyCode == Keys.D1)
+            {
+                // Đặt hình ảnh người chơi là thùng rác 1
+                UEHer.Image = can1;
+                // Cập nhật loại thùng rác hiện tại
+                canType = 0;
+            }
+
+            // Xử lý khi người dùng chọn loại thùng rác 2 (D2)
+            if (e.KeyCode == Keys.D2)
+            {
+                // Đặt hình ảnh người chơi là thùng rác 2
+                UEHer.Image = can2;
+                // Cập nhật loại thùng rác hiện tại
+                canType = 1;
+            }
+
+            // Xử lý khi người dùng chọn loại thùng rác 3 (D3)
+            if (e.KeyCode == Keys.D3)
+            {
+                // Đặt hình ảnh người chơi là thùng rác 3
+                UEHer.Image = can3;
+                // Cập nhật loại thùng rác hiện tại
+                canType = 2;
+            }
+        }
+
+        // Phương thức này tải các dữ liệu liên quan
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Tải hình ảnh
+            r1 = Image.FromFile(@"C:\Users\nhatl\source\repos\CSLT\Imagess\r1.png");
+            r2 = Image.FromFile(@"C:\Users\nhatl\source\repos\CSLT\Imagess\r2.png");
+            r3 = Image.FromFile(@"C:\Users\nhatl\source\repos\CSLT\Imagess\r3.png");
+            r4 = Image.FromFile(@"C:\Users\nhatl\source\repos\CSLT\Imagess\r4.png");
+
+            nr1 = Image.FromFile(@"C:\Users\nhatl\source\repos\CSLT\Imagess\nr1.png");
+            nr2 = Image.FromFile(@"C:\Users\nhatl\source\repos\CSLT\Imagess\nr2.png");
+            nr3 = Image.FromFile(@"C:\Users\nhatl\source\repos\CSLT\Imagess\nr3.png");
+            nr4 = Image.FromFile(@"C:\Users\nhatl\source\repos\CSLT\Imagess\nr4.png");
+
+
+            o1 = Image.FromFile(@"C:\Users\nhatl\source\repos\CSLT\Imagess\o1.png");
+            o2 = Image.FromFile(@"C:\Users\nhatl\source\repos\CSLT\Imagess\o2.png");
+            o3 = Image.FromFile(@"C:\Users\nhatl\source\repos\CSLT\Imagess\o3.png");
+            o4 = Image.FromFile(@"C:\Users\nhatl\source\repos\CSLT\Imagess\o4.png");
+
+            // Tải âm thanh
+            catchSound = new SoundPlayer(@"C:\Users\nhatl\source\repos\CSLT\Imagess\catch.wav");
+            missSound = new SoundPlayer(@"C:\Users\nhatl\source\repos\CSLT\Imagess\miss1.wav");
+            backgroundMusic = new SoundPlayer(@"C:\Users\nhatl\source\repos\CSLT\Imagess\background1.wav");
+
+            // Khởi tạo mảng 2 chiều lưu trữ hình ảnh các loại rác
+            rubbishes = new Image[3, 4]{
+                { r1, r2, r3, r4},
+                {o1, o2, o3, o4},
+                {nr1, nr2, nr3, nr4},
+            };
+
+            // Tải hình ảnh các loại thùng rác
+            can1 = Image.FromFile(@"C:\Users\nhatl\source\repos\CSLT\Imagess\can1.png");
+            can2 = Image.FromFile(@"C:\Users\nhatl\source\repos\CSLT\Imagess\can2.png");
+            can3 = Image.FromFile(@"C:\Users\nhatl\source\repos\CSLT\Imagess\can3.png");
+        }
+
+        private void Exit_Click(object sender, EventArgs e)
+        {
+            // Dừng nhạc nền
+            backgroundMusic.Stop();
+
+            // Thiết lập lại trạng thái trò chơi
+            isGameStarted = false;
+
+            // Ẩn các thành phần liên quan đến Game Over
+            GameOver.Visible = false;
+            FinalScore.Visible = false;
+            Exit.Visible = false;
+
+            // Hiển thị lại các thành phần ban đầu
+            Start.Visible = true;
+            Sorting.Visible = true;
+
+            // Reset điểm số và số lần bỏ lỡ
+            score = 0;
+            missed = 0;
+        }
+
+        private void restartGame()
+        {
+
+            // Ẩn nút "Thoát" (có thể tùy chỉnh)
+            Exit.Visible = false;
+
+            // Thiết lập loại rác mặc định (có thể tùy chỉnh)
+            canType = 1;
+
+            // Khởi tạo lại trạng thái di chuyển (bỏ chọn trái/phải)
             goLeft = false;
             goRight = false;
 
-            MainTimer.Start();  
+            // Bắt đầu bộ hẹn giờ trò chơi
+            MainTimer.Start();
+
+            // Lặp qua tất cả các PictureBox có tag "rubbish"
+            foreach (Control x in this.Controls)
+            {
+                if (x is PictureBox && ((string)x.Tag == "rubbish:0") || ((string)x.Tag == "rubbish:1") || ((string)x.Tag == "rubbish:2"))
+                {
+                    // Vị trí lại các rác ở ngoài màn hình phía trên
+                    x.Top = random.Next(80, 300) * -1;
+                    // Đặt lại vị trí ngang ngẫu nhiên cho các rác
+                    x.Left = random.Next(5, this.ClientSize.Width - 5 - x.Width);
+                }
+            }
         }
     }
 }
